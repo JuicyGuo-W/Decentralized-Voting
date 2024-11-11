e1d2b2dcefccc364dd3ebc4c5a03e14e70b0e25b
JuicyGuo-W <milie25@foxmail.com>
2024-11-11 18:22:37 -0500

    finished step 1

--- src/components/ConnectWallet.js
+++ src/components/ConnectWallet.js
@@ -1,15 +1,13 @@
 import React, { useState } from 'react';
-import { ethers } from 'ethers';
+import { ethers } from 'ethers'; 
 
 const ConnectWallet = () => {
   const [walletAddress, setWalletAddress] = useState("");
 
-  // Function to connect MetaMask wallet
   const connectWallet = async () => {
     if (window.ethereum) {
       try {
-        await window.ethereum.request({ method: 'eth_requestAccounts' });
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const provider = new ethers.BrowserProvider(window.ethereum);
         const signer = provider.getSigner();
         const address = await signer.getAddress();
         setWalletAddress(address);
